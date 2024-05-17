b9779efae8c7acf92374e4d69e811813fde7614b
juban1234 <alexzo8677gmail.com>
2024-05-17 16:46:07 -0500

    decoracion 2.3

--- js/app.js
+++ js/app.js
@@ -2,58 +2,82 @@ function piedra() {
     let resultado = CPU();
     let mensaje = "";
     if (resultado == 0) {
-        mensaje = "Empate - Ambos sacaron piedra";
+     mensaje = "Empate - Ambos sacaron piedra";
     } else if (resultado == 1) {
-        mensaje = "Perdiste - La CPU sacó papel";
+     mensaje = "Perdiste - La CPU sacó papel";
+     derrotas++; 
     } else if (resultado == 2) {
-        mensaje = "Ganaste - La CPU sacó tijera";
+     mensaje = "Ganaste - La CPU sacó tijera";
+     victorias++; 
     }
+    
+    actualizarContadores();
+    verificarGanador(); 
     document.getElementById("resultado").textContent = mensaje;
-}
+ }
 
-document.addEventListener('DOMContentLoaded', function() {
-    var botonPi = document.getElementById('piedra');
-    botonPi.addEventListener('click', piedra);
-});
+
  
 function papel() {
     let resultado = CPU();
     let mensaje = "";
     if (resultado == 0) {
     mensaje = "Ganaste - La CPU sacó piedra";
+    victorias++;
     } else if (resultado == 1) {
     mensaje = "Empate - Ambos sacaron papel";
     } else if (resultado == 2) {
     mensaje = "Perdiste - La CPU sacó tijera";
+    derrotas++; 
     }
+    actualizarContadores();
+    verificarGanador(); 
     document.getElementById("resultado").textContent = mensaje;
 }
- 
-document.addEventListener('DOMContentLoaded', function() {
-    var botonPa = document.getElementById('papel');
-    botonPa.addEventListener('click', papel);
-});
  
 function tigera() {
     let resultado = CPU();
     let mensaje = "";
     if (resultado == 0) {
     mensaje = "Perdiste - La CPU sacó piedra";
+    derrotas++; 
     } else if (resultado == 1) {
     mensaje = "Ganaste - La CPU sacó papel";
+    victorias++;
     } else if (resultado == 2) {
     mensaje = "Empate - Ambos sacaron tijera";
     }
+    actualizarContadores();
+    verificarGanador(); 
     document.getElementById("resultado").textContent = mensaje;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    var botonTi = document.getElementById('tigera');
-    botonTi.addEventListener('click', tigera);
-});
  
 function CPU() {
     let CPU = Math.floor(Math.random() * 3);
     return CPU;
 }
+
+let victorias = 0;
+let derrotas = 0;
+
+function actualizarContadores() {
+   document.getElementById("victorias").textContent = "Victorias: " + victorias;
+   document.getElementById("derrotas").textContent = "Derrotas: " + derrotas;
+}
+
+function verificarGanador() {
+    if (victorias >= 3) {
+        alert("¡Felicidades, has ganado!");
+        reiniciarJuego();
+    } else if (derrotas >= 3) {
+        alert("Lo siento, has perdido.");
+        reiniciarJuego();
+    }
+}
  
+function reiniciarJuego() {
+    victorias = 0;
+    derrotas = 0;
+    actualizarContadores();
+}
